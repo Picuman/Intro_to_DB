dc300eb27f644e04efb9fd9ce788f0d57b4e4d38
Firass Zaoui <109809162+Picuman@users.noreply.github.com>
2025-10-19 22:20:52 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,16 @@
-USE alx_book_store;
-SHOW CREATE TABLE books;
+-- Script to print full description of the books table
+-- Without using DESCRIBE or EXPLAIN
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books'
+ORDER BY
+    ORDINAL_POSITION;
