8a7566cdab44d684968edec3818573a13d1d50ef
Firass Zaoui <109809162+Picuman@users.noreply.github.com>
2025-10-19 21:48:43 +0100

    Fix Order_Details table definition and primary key

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,7 +34,11 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create Order_Details table
 CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY
-
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
